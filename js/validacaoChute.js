c1fe38a9b4f1183d7bdeec3b710b5e5a0532372d
Letícia do Vale <leticiadovale.o2@gmail.com>
2023-05-04 16:37:51 -0300

    first commit

---
+++ js/validacaoChute.js
@@ -0,0 +1,40 @@
+function validacaoChute(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)) {
+        elementoChute.innerHTML += ' <div> Valor inválido </div>'
+        return
+    }
+
+    if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
+        elementoChute.innerHTML += ' <div> Valor inválido: O número precisa estar entre 1 e 1000 </div>'
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = ` 
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}
+
+            <div>
+            <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button> </div>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += '<div>O número secreto é menor <i class="fa-solid fa-arrow-down"></i></div>'
+    } else {
+        elementoChute.innerHTML += '<div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>'
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroForMaiorOuMenorQueOValorPermitido(numero) {
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente')
+    window.location.reload()
+})
